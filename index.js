7e2a637a4a13b4daedeedc9bdb057ddf0b09a891
jden <jason@denizac.org>
2015-02-11 13:47:44 -0800

    change api: resolved is flat, resolved.deep is deep

--- index.js
+++ index.js
@@ -1,22 +1,64 @@
-require('polyfill-promise')
+var Promise = require('polyfill-promise').noConflict()
 var refcount = require('refcount')
 
 function isThenable (x) {
   return x !== null &&
          typeof x === 'object' &&
-         typeof x.then === 'function';
+         typeof x.then === 'function'
 }
 
-module.exports = function resolved (obj) {
+module.exports = resolved
+module.exports.deep = resolvedDeep
+
+function resolved(obj) {
+  var promises = []
+  var val
+
+  for (var key in obj) {
+    val = obj[key]
+
+    if (isThenable(val)) {
+      promises.push(val)
+    }
+  }
+
+  if (promises.length === 0) {
+    return Promise.resolve(obj)
+  }
+
+  return Promise.all(promises).then(function (vals) {
+
+    var out = {}
+    var val
+
+    for (var key in obj) {
+      val = obj[key]
+
+      if (isThenable(val)) {
+        out[key] = vals[promises.indexOf(val)]
+      } else {
+        out[key] = val
+      }
+    }
+
+    return out
+
+  })
+
+}
+
+
+
+function resolvedDeep (obj) {
   return new Promise(function (resolve, reject) {
 
     var promises = []
-      , counter
-      , stack = [[{val: obj}, 'val']]
-      , n
-      , key
-      , val
-      , visited = [];
+    var counter
+    var stack = [[{val: obj}, 'val']]
+    var n
+    var key
+    var val
+    var visited = []
 
     // walk the object and stash all the promises
     while (n = stack.pop()) {
@@ -24,30 +66,31 @@ module.exports = function resolved (obj) {
       val = n[0][key]
       if (visited.indexOf(val) >= 0) {
         // break cycles
-        continue;
+        continue
       }
       visited.push(val);
 
       if (isThenable(val)) {
-        promises.push(n);
+        promises.push(n)
       } else {
         for(var key in val) {
           if (val.hasOwnProperty(key)) {
-            stack.push([val, key]);
+            stack.push([val, key])
           }
         }
       }
     }
 
     // attach resolve handlers
-    counter = refcount(promises.length);
+    counter = refcount(promises.length)
 
     promises.forEach(function (p) {
       var key = p[1]
-        , promise = p[0][key]
+      var promise = p[0][key]
+      
       promise.then(function resolve(value) {
         // update in place
-        p[0][key] = value;
+        p[0][key] = value
         counter.pop()
       }, reject)
     })
@@ -62,4 +105,4 @@ module.exports = function resolved (obj) {
     }
   })
 
-};
+}
--- test/test.js
+++ test/test.js
@@ -1,32 +1,10 @@
 var chai = require('chai')
 chai.should()
-require('polyfill-promise')
+var Promise = require('polyfill-promise').noConflict()
 
 var resolved = require('../index')
 
 describe('resolved', function () {
-
-  it('example', function (done) {
-    var travelPlans = {
-      destination: Promise.resolve('Indonesia'),
-      departure: new Date('June 4, 2013'),
-      travelers: [
-        'jden',
-        Promise.resolve('a friend')
-      ],
-      accommodations: {
-        hotel: 'nah',
-        bungalow: Promise.resolve('right on')
-      }
-    }
-
-    resolved(travelPlans).then(function (travelPlans) {
-      travelPlans.destination.should.equal('Indonesia')
-      travelPlans.travelers.should.deep.equal(['jden','a friend'])
-      travelPlans.accommodations.bungalow.should.equal('right on')
-    }).then(done, done)
-
-  })
 
   it('fulfills a plain value that does not have any promises in its object graph', function (done) {
     var obj = {
@@ -50,5 +28,45 @@ describe('resolved', function () {
 
   })
 
+  it('resolves promise-properties of an object', function (done) {
+    var obj = {
+      foo: null,
+      bar: 1,
+      baz: Promise.resolve(true)
+    }
+
+    resolved(obj).then(function (x) {
+      x.should.deep.equal({
+        foo: null,
+        bar: 1,
+        baz: true
+      })
+    }).then(done, done)
+  })
+
+  describe('.deep', function () {
+    it('example', function (done) {
+    var travelPlans = {
+      destination: Promise.resolve('Indonesia'),
+      departure: new Date('June 4, 2013'),
+      travelers: [
+        'jden',
+        Promise.resolve('a friend')
+      ],
+      accommodations: {
+        hotel: 'nah',
+        bungalow: Promise.resolve('right on')
+      }
+    }
+
+    resolved.deep(travelPlans).then(function (travelPlans) {
+      travelPlans.destination.should.equal('Indonesia')
+      travelPlans.travelers.should.deep.equal(['jden','a friend'])
+      travelPlans.accommodations.bungalow.should.equal('right on')
+    }).then(done, done)
+
+  })
+
+  })
 
 })
